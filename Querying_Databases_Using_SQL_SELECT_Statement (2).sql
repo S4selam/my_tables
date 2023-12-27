844e204d8312c9724542c0e16f4c5182a012e388
S4selam <149325381+S4selam@users.noreply.github.com>
2023-12-27 04:44:18 -0800

    Update Querying_Databases_Using_SQL_SELECT_Statement (2).sql

--- Querying_Databases_Using_SQL_SELECT_Statement (2).sql
+++ Querying_Databases_Using_SQL_SELECT_Statement (2).sql
@@ -11,19 +11,21 @@
 #############################
 
 -- Retrieve all data from the employees table
-
+SELECT*
+FRPM employees;
 
 
 -- Retrieve all data from the departments table
-
+SELECT* 
+FROM departments;
 
 
 -- Retrieve all data from the departments employees table
-
+SELECT* FRPM dept_emp;
 
 
 -- Retrieve all data from the salaries table
-
+SELECT* FROM salaries
 
 
 #############################
@@ -36,35 +38,34 @@
 -- SELECT - FROM
 
 -- 2.1: Select the first name and last name of all employees
-
-
-
+SELECT first_name, last_name
+FROM employees;
 
 -- 2.2 Select all records from the employees table
 SELECT * FROM employees;
 
 
-
 -- 2.3 (Ex.) Select the department number from the departments table
-
-
-
+SELECT dept_no
+FROM departments;
 
 -- 2.4 (Ex.) Select all records from the departments table
 SELECT * FROM departments;
-
-
 
 ############
 -- SELECT FROM - WHERE
 
 -- 2.5 Select all employees whose first name is 'Elvis'
 
-
+SELECT*
+FROM employees
+WHERE first_name 'Elvis';
     
 -- 2.6 (Ex.) Select all employees whose first name is 'Hilari'
 
-
+SELECT*
+FROM employees
+WHERE first_name 'Hilari';
     
 
 #############################
@@ -79,11 +80,16 @@ SELECT * FROM departments;
 
 -- 3.1 Select all male employees whose first name is 'Denis'
 
+SELECT*
+FROM employees
+WHERE first_name = 'Denis' AND gender = 'M'
 
 
   
 -- 3.2 (Ex.) Select all female employees whose last name is 'Reistad'
-
+SELECT *
+FROM Employees
+WHERE gender = 'F' AND Last_name = 'Reistad'
 
 
 
@@ -97,7 +103,9 @@ FROM employees
 WHERE first_name = 'Denis' AND gender = 'M';
 
 -- 3.3 Select all employees whose first name is 'Denis' or 'Elvis'
-
+SELECT *
+FROM employees
+WHERE first_name = 'Denis' or first_name = 'Elvis'
 
 
 
@@ -111,7 +119,9 @@ WHERE first_name = 'Denis' AND first_name = 'Elvis';
 -- 3.4 (Ex.) Retrieve a list of all employees whose first name is either
 'Kellie' or 'Aruna'
 
-
+SELECT *
+FROM employees
+WHERE frist_name = 'Killie' or first_name = 'Aruna'
 
 
 
@@ -126,12 +136,16 @@ WHERE last_name = 'Denis' AND gender = 'M' OR gender = 'F';
 
 -- Correct Syntax
 
-
+SELECT *
+FROM employees
+WHERE last_name = Denis and (gender = 'M' or gender = 'F');
 
 
 -- 3.6 (Ex.) Retrieve a list of all female employees whose first name is either
 'Kellie' or 'Aruna'
-
+SELECT *
+FROM employees
+WHERE (first_name = 'Kellie' or first_name = 'Aruna') AND gender = 'F'
 ###########
 -- IN / NOT IN
 
@@ -142,16 +156,14 @@ FROM employees
 WHERE first_name = 'Cathie' OR first_name = 'Mark' OR first_name = 'Nathan';
 
 -- Alternative Solution
-
-
-
-
+SELECT *
+FROM employees 
+WHERE first_name IN ('Cathie', 'Mark' or 'Nathan')
 
 -- NOT IN
-
-
-
-
+SELECT *
+FROM employees 
+WHERE first_name NOT IN ('Cathie', 'Mark' or 'Nathan')
 
 #############################
 -- Task Four: Wildcard Characters 
@@ -163,20 +175,20 @@ WHERE first_name = 'Cathie' OR first_name = 'Mark' OR first_name = 'Nathan';
 -- LIKE / NOT LIKE 
 
 -- 4.1: Extract a list of all employees whose first name starts with 'Mar'
-
-
-
+SELECT *
+FROM employees 
+WHERE first_name LIKE ('Mar%');
 
 -- 4.2: Extract a list of all employees whose first name starts with 'Ar'
-
-
-
+SELECT *
+FROM employees 
+WHERE first_name LIKE ('Ar');
 
 
 -- 4.3: Extract a list of all employees whose first name ends with 'Ar'
-
-
-
+SELECT *
+FROM employees 
+WHERE first_name LIKE ('%Ar');
 
 
 -- 4.4: What do you think the result of this query will be?
@@ -199,6 +211,9 @@ WHERE first_name NOT LIKE ('%Mar%');
 
 -- 4.7 (Ex.): Extract all individuals from the 'employees' table whose first name 
 -- starts with 'Mark'
+SELECT *
+FROM employees 
+WHERE first_name LIKE ('Mark%');
 
     
 -- 4.8 (Ex.): Extract all individuals from the employees table whose first name 
@@ -221,25 +236,36 @@ WHERE first_name NOT LIKE ('%Jack%');
 -- 5.1: Extract a list of all employees who were employed between
 -- 1st of January, 1990 and 1st January, 2000
  
-
+SELECT *
+FROM employees 
+WHERE hire_date BETWEEN '1990-01-01' and '2000-01-01';
 
 
     
--- 5.2: Extract a list of all employees who were not employed between
--- 1st of January, 1990 and 1st January, 2000
+-- 5.2: Extract a list of all employees who were not employed between 
+	1st of January, 1990 and 1st January, 2000
+ 
+SELECT *
+FROM employees 
+WHERE hire_date NOT BETWEEN '1990-01-01' and '2000-01-01';
 
 
 
 	
 -- 5.3 (Ex.): Select all the records from the salaries table, regarding contracts
 -- from 66000 to 70000 dollars/year
+SELECT *
+FROM salaries
+WHERE salary BETWEEN '66000' and '70000';
 
 -- Selecting all records from the salaries table
 
 SELECT * FROM salaries;
 
 -- Solution to 5.3
-
+SELECT *
+FROM salaries
+WHERE salary BETWEEN '66000' and '70000';
 
 
 
@@ -247,8 +273,9 @@ SELECT * FROM salaries;
 -- 5.4: Retrieve a list of individuals whose emp_no is not between
 -- 10004 and 10012.
 
-
-
+SELECT *
+FROM salaries
+WHERE emp_no NOT BETWEEN '10004' and '10012'
 
 
 ###########
@@ -257,21 +284,28 @@ SELECT * FROM salaries;
 -- 5.5: Extract a list of employees whose first name is not null
    
 -- Let us count how many first names are in the employees table
-
+SELECT count first_name
+FROM employees;
 
 
 -- Solution to 5.5
-
+SELECT *
+FROM employees
+WHERE first_name IS NOT NUL;
 
 
 -- 5.6: Extract a list of employees whose first name is null
-    
+ SELECT *
+FROM employees
+WHERE first_name IS NUL;
 
 
 
 
 -- 5.7 (Ex.): Select the names of all departments whose department number value is not null
-
+SELECT dept_no
+FROM departments
+WHERE dept_no IS NOT NUL;
 
 
 
@@ -292,12 +326,17 @@ FROM employees
 WHERE first_name = 'Mark';
 
 -- 6.2: Retrieve a list of all employees whose first name is not 'Mark'
-    
+SELECT*
+FROM employees
+WHERE frist_name != 'Mark';
+	
 
 
 
 -- 6.3: Retrieve a list of all employees whose first name is not 'Mark'
-
+SELECT*
+FROM employees
+WHERE frist_name <> 'Mark';
 
 
 
@@ -305,19 +344,21 @@ WHERE first_name = 'Mark';
 -- Greater than & Less than
 
 -- 6.4: Retrieve a list of all employees who were employed after 1st of January, 1997.
-    
+SELECT*
+FROM employees
+WHERE hire_date > '1997-01-01';
 
 
   
 -- 6.5: Retrieve a list of all employees who were employed on or after 1st of January, 1997.
-
-
-
+SELECT*
+FROM employees
+WHERE hire_date >= '1997-01-01'; 
 
 -- 6.6: Retrieve a list of all employees who were employed before 1st of February, 1990
-
-
-
+SELECT*
+FROM employees
+WHERE hire_date < '1990-01-01';
 
 -- 6.7: What do you think the result will be??
 SELECT *
@@ -325,14 +366,14 @@ FROM employees
 WHERE hire_date <= '1990-02-01';
 
 -- 6.8 (Ex.): Retrieve a list of all female employees who were hired on or after 1st January, 1999. 
-
-
-
+SELECT*
+FROM employees
+WHERE hire_date >= '1999-01-01'; AND (gender = 'F');
 
 -- 6.9: Extract a list with employees' salaries higher than 50,000 dollars per annum
-
-
-
+SELECT*
+FROM salaries
+WHERE salary > 50000;
 
 #############################
 -- Task Seven: Select Distinct, Order BY, Limit, and SQL Aliases.
@@ -351,12 +392,14 @@ SELECT gender
 FROM employees;
 
 -- Solution to 7.1
-
+SELECT DISTINCT gender
+FROM employees;
 
 
 
 -- 7.2: Retrieve a list of the different first names in the employees database
-
+SELECT DISTINCT first_name
+FROM employees;
 
 
     
@@ -365,7 +408,11 @@ FROM employees;
 
 -- 7.3: Extract a list with employees' salaries higher than 50,000 dollars per annum
 -- Sort the list by the most paid employee
-
+SELECT*
+FROM salaries
+WHERE salary > 50000
+ORDER BY salary DESC;
+	
 
 
 
@@ -376,7 +423,9 @@ ORDER BY first_name;
 
 -- 7.5: Select all records and order by first name in descending order
 
-
+SELECT *
+FROM employees
+ORDER BY first_name DESC;
 
 
 -- 7.6: Select all records and order by first name and last name
@@ -391,8 +440,10 @@ ORDER BY first_name, last_name ASC;
 -- 7.7: Retrieve a list of the first 100 records in the employees table 
 -- and order by emp_no in ascending order
 
-
-
+SELECT *
+FROM employees
+ORDER BY emp_no ASC
+LIMIT 100;
 
 -- From 5.5: We counted how many first names are in the employees table
 
@@ -402,11 +453,15 @@ FROM employees;
 -- 7.8: How many firstname are in the employees table. Rename the result as
 -- FirstName_Count
 
-
+SELECT COUNT(first_name) AS FirstName_Count
+FROM employees;
 
 
 -- Let's count how many distinct first names we have in the employees table
-
+SELECT DISTINCT first_name, COUNT(*) As names_counts
+FROM employees;
+GROUP BY first_name
+ORDER BY first_name;
 
 
 
